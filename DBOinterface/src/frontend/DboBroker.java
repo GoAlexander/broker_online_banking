7131d0a874c9d1b85e3fba7f36bfc68536b31ba3
GoAlexander <AlexanderDydychkin@yandex.ru>
2015-11-13 23:48:25 +0300

    Excel parsing fix (now it works)

--- DBOinterface/src/frontend/DboBroker.java
+++ DBOinterface/src/frontend/DboBroker.java
@@ -97,7 +97,7 @@ class DboBroker {
   }   
   
   
-  public static void main(String args[]) {   
+  public static void main(String args[]) throws Exception {   
     // Create the frame on the event dispatching thread.
 	
 	//Test.Begin.
