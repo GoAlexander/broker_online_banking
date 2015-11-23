6f571baf3bb2cf3aaeaba05f7953642c3366ed9a
GoAlexander <AlexanderDydychkin@yandex.ru>
2015-11-23 13:56:49 +0300

    minor commit

--- DBOinterface/src/backend/Parsing.java
+++ DBOinterface/src/backend/Parsing.java
@@ -41,20 +41,72 @@ public class Parsing {
 	 * 
 	 * //XSSFWorkBook, XSSFSheet, XSSFRow
 	 * 
+	 * 
+	 * -----
+	 * public static void main(String[] args) throws Exception {
+        InputStream in = new FileInputStream("/tmp/1.xls");
+        HSSFWorkbook wb = new HSSFWorkbook(in);
+ 
+        Sheet sheet = wb.getSheetAt(0);
+        Iterator<Row> it = sheet.iterator();
+        while (it.hasNext()) {
+            Row row = it.next();
+            Iterator<Cell> cells = row.iterator();
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                int cellType = cell.getCellType();
+                switch (cellType) {
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue() + "=");
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print("[" + cell.getNumericCellValue() + "]");
+                        break;
+ 
+                    case Cell.CELL_TYPE_FORMULA:
+                        System.out.print("[" + cell.getNumericCellValue() + "]");
+                        break;
+                    default:
+                        System.out.print("|");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+    }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
 	 */
 
 	public static void readtest() throws Exception {
 		//Achtung! Придумать другой путь к xls!!! 
+		
+		//Achtung! Проверять строчку НБД банка!!!
 		InputStream in = new FileInputStream("/home/alexander/Files/Workspaces/eclipse/broker_online_banking/DBexcel/физ_лица.xls");
 		
 		
 		HSSFWorkbook wb = new HSSFWorkbook(in);
 		ExcelExtractor extractor = new ExcelExtractor(wb);
-		extractor.setFormulasNotResults(false); // Считать формулы
-		extractor.setIncludeSheetNames(true);
+		//extractor.setFormulasNotResults(false); // Считать формулы
+		//extractor.setIncludeSheetNames(true); //зачем?
 		String text = extractor.getText();
-
+		
+		
 		System.out.println(text);
+		
+		//cell output
+
+        //Cell cell = cells.next();  
+        //System.out.print(cell.getStringCellValue());
+		
+		//CellReference cellReference = new CellReference("B3"); 
+		//Row row = sheet.getRow(cellReference.getRow());
+		//Cell cell = row.getCell(cellReference.getCol()); 
+		
 
 	}
 
